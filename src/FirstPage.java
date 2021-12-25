52807dc468933281c207dbf9ec885832dfd37dc0
Tim Fan <tim61114@gmail.com>
2021-12-25 12:08:46 -0800

    minor tweaks

--- src/FirstPage.java
+++ src/FirstPage.java
@@ -15,26 +15,23 @@ public class FirstPage {
             @Override
             public void actionPerformed(ActionEvent e) {
                 num = Integer.parseInt(numField.getText());
-                System.out.println(num);
                 JFrame frame2 = new JFrame();
-                frame2.setContentPane(new MainPage(num).MainPanel);
-                frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame2.setBounds(600,600,num*50,300);
+                MainPage mp = new MainPage(num);
+                frame2.setContentPane(mp.MainPanel);
+                frame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                frame2.setBounds(600,450,num*50,300);
                 frame2.setVisible(true);
             }
         });
     }
 
-    public void getNum(){
-         num = Integer.parseInt(numField.getText());
-    }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("APP");
+        JFrame frame = new JFrame("DRAW DRAW DRAW");
         FirstPage fp = new FirstPage();
         frame.setContentPane(fp.MainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBounds(600,600,400,300);
+        frame.setBounds(600,450,400,300);
         frame.setVisible(true);
     }
 
--- src/MainPage.java
+++ src/MainPage.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static javax.swing.JOptionPane.DEFAULT_OPTION;
+
 public class MainPage {
     private JButton pollButton;
     private JLabel label;
@@ -9,19 +11,21 @@ public class MainPage {
 
     public MainPage(int n) {
         Main m = new Main(n);
+        Icon icon = new ImageIcon("gift.jpeg");
+
         pollButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int temp = m.poll();
                 if(temp != 0){
-                    JOptionPane.showMessageDialog(null,"The next lucky person is number "+temp+" !");
+                    JOptionPane.showMessageDialog(null,"The next person is number "+temp+" !",null,DEFAULT_OPTION,null);
                     label.setText(label.getText() + (temp) + " ");
 
                 } else {
                     JOptionPane.showMessageDialog(null,"Done!");
                 }
-
             }
         });
     }
+
 }
